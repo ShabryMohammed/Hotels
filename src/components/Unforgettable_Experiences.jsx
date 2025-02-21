d4555681827cfce88acca862366e441494926c57
it21296796 <99311998+it21296796@users.noreply.github.com>
2025-02-21 12:27:58 +0530

    image adjusments

--- src/components/Unforgettable_Experiences.jsx
+++ src/components/Unforgettable_Experiences.jsx
@@ -12,30 +12,30 @@ export default function UnforgettableExperiences() {
         <div className="mt-[45px] sm:mt-[90px] max-w-[1200px] w-full flex flex-col sm:flex-row flex-wrap gap-[10px] sm:gap-[14px] justify-center">
           {/* First column */}
           <div className="flex flex-col gap-[10px] sm:gap-[14px]">
-            <img src="/public/images/UE_1.png" alt="Luxury hotel room" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
-            <img src="/public/images/UE_6.png" alt="Resort exterior" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
-            <img src="/public/images/UE_9.png" alt="Modern hotel room" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+            <img src="/images/UE_1.png" alt="Luxury hotel room" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+            <img src="/images/UE_6.png" alt="Resort exterior" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+            <img src="/images/UE_9.png" alt="Modern hotel room" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
           </div>
   
           {/* Second column */}
           <div className="flex flex-col gap-[10px] sm:gap-[14px]">
-            <img src="/public/images/UE_2.png" alt="Elegant interior" className="w-full sm:w-[225px] h-[350px] sm:h-[526px] object-cover" />
-            <img src="/public/images/UE_10.png" alt="Elegant interior" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+            <img src="/images/UE_2.png" alt="Elegant interior" className="w-full sm:w-[225px] h-[350px] sm:h-[526px] object-cover" />
+            <img src="/images/UE_10.png" alt="Elegant interior" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
           </div>
   
           {/* Third column */}
           <div className="flex flex-col gap-[10px] sm:gap-[14px]">
-            <img src="/public/images/UE_3.png" alt="Indoor pool" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
-            <img src="/public/images/UE_7.png" alt="Spa amenities" className="w-full sm:w-[225px] h-[350px] sm:h-[527px] object-cover" />
+            <img src="/images/UE_3.png" alt="Indoor pool" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+            <img src="/images/UE_7.png" alt="Spa amenities" className="w-full sm:w-[225px] h-[350px] sm:h-[527px] object-cover" />
           </div>
   
           {/* Fourth column */}
           <div className="flex flex-col gap-[10px] sm:gap-[14px]">
             <div className="flex flex-col sm:flex-row gap-[10px] sm:gap-[18px]">
-              <img src="/public/images/UE_4.png" alt="Outdoor pool area" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
-              <img src="/public/images/UE_5.png" alt="Formal dining room" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+              <img src="/images/UE_4.png" alt="Outdoor pool area" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
+              <img src="/images/UE_5.png" alt="Formal dining room" className="w-full sm:w-[225px] h-[170px] sm:h-[256px] object-cover" />
             </div>
-            <img src="/public/images/UE_8.png" alt="Towels and spa oils" className="w-full sm:w-[466px] h-[350px] sm:h-[526px] object-cover" />
+            <img src="/images/UE_8.png" alt="Towels and spa oils" className="w-full sm:w-[466px] h-[350px] sm:h-[526px] object-cover" />
           </div>
         </div>
       </div>
